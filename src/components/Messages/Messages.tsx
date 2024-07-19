40df351b4a66e9abaaed435e0c80273b5090660a
Galkon <Galkon@users.noreply.github.com>
2024-07-19 11:49:45 -0600

    fix thinking message hide/show with pending content

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -122,12 +122,17 @@ const Messages = () => {
     >
       <div className={styles.messages}>
         {messages.length > 0 &&
-          messages.map((message, index) => (
-            <Message key={index} message={message} />
-          ))}
+          messages.map((message, index) => {
+            if (message.type === 'assistant' && !message.content?.length) {
+              return ''
+            }
+            return <Message key={index} message={message}/>
+          })
+        }
         {inputIsDisabled &&
           (!messages.length ||
-            messages[messages.length - 1].type !== "assistant") && (
+            messages[messages.length - 1].type !== "assistant" ||
+            !messages[messages.length - 1].content?.length) && (
             <ThinkingMessage />
           )}
       </div>
