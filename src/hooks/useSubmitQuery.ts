ec2543a387fffdfeaa8bf087d94eb944a3451d94
Samantha Pelletier <sam@medal.tv>
2024-07-26 09:17:27 -0400

    add context on initial chat request

--- src/hooks/useSubmitQuery.ts
+++ src/hooks/useSubmitQuery.ts
@@ -244,8 +244,12 @@ export const useSubmitQuery = () => {
       }));
       formData.append("previous_messages", JSON.stringify(previousMessages));
 
-      let contextString =
-        "This is a new conversation with Highlight Chat. You do not have any Highlight Context available.";
+      let contextString = prepareHighlightContext(context);
+
+      if (contextString.trim() === "") {
+        contextString =
+          "This is a new conversation with Highlight Chat. You do not have any Highlight Context available.";
+      }
 
       console.log("contextString:", contextString);
       formData.append("context", contextString);
