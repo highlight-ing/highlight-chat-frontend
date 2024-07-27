f197a76a1676d8e8563047c0f17f7ed417e11bb9
Galkon <Galkon@users.noreply.github.com>
2024-07-26 19:40:49 -0600

    Update DeleteChatModal.tsx

--- src/presentations/modals/DeleteChatModal.tsx
+++ src/presentations/modals/DeleteChatModal.tsx
@@ -4,16 +4,12 @@ import {ChatHistoryItem, ModalObjectProps} from "@/types";
 import styles from './modals.module.scss'
 import Button from "@/components/Button/Button";
 
-interface ChatContext {
-  context: ChatHistoryItem
-}
-
-const DeleteChatModal = ({id, context}: ModalObjectProps & ChatContext) => {
-  console.log('style:', styles.deleteChat)
+const DeleteChatModal = ({id, context}: ModalObjectProps) => {
+  const chat = context as ChatHistoryItem
   return (
     <Modal id={id} size={'small'} header={'Are you sure?'} bodyClassName={styles.deleteChat}>
       <span><span className="text-red-400 font-medium">Warning:</span> Deleting this chat cannot be undone:</span>
-      <span className="font-medium">{context.title}</span>
+      <span className="font-medium">{chat.title}</span>
       <Button size={'medium'} variant={'secondary'}>
         Delete
       </Button>
