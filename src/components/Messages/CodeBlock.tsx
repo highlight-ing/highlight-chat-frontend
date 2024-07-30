a9e00809a0428c7fdd70cc792a396948b54f1fbd
Galkon <Galkon@users.noreply.github.com>
2024-07-29 19:26:26 -0600

    copied! transition upon copying code

--- src/components/Messages/CodeBlock.tsx
+++ src/components/Messages/CodeBlock.tsx
@@ -1,25 +1,46 @@
-import {PropsWithChildren} from "react";
+import {PropsWithChildren, useEffect, useRef, useState} from "react";
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import {darcula} from "react-syntax-highlighter/dist/esm/styles/hljs";
 import Button from "@/components/Button/Button";
 import styles from './message.module.scss'
-import {Copy} from "iconsax-react";
+import {Copy, TickCircle} from "iconsax-react";
 
 interface CodeBlockProps {
   language: string
 }
 
 const CodeBlock = ({children, language}: PropsWithChildren<CodeBlockProps>) => {
+  const [isCopied, setCopied] = useState(false)
+  const timeoutRef = useRef<NodeJS.Timeout>()
   const code = children as (string | string[])
+
   const onCopy = () => {
+    setCopied(true)
     navigator.clipboard.writeText(typeof code === 'string' ? code : code.join('\n'))
+    timeoutRef.current = setTimeout(() => {
+      setCopied(false)
+      timeoutRef.current = undefined
+    }, 2000)
   }
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+    }
+  }, [])
+
   return (
     <div className={styles.codeBlock}>
       <div className={styles.codeBlockHeader}>
         {language}
         <Button size={'small'} variant={'ghost-neutral'} onClick={onCopy}>
-          <Copy variant={"Linear"} size={16}/> Copy Code
+          {
+            isCopied
+              ? <><TickCircle variant={"Linear"} size={16}/> Copied!</>
+              : <><Copy variant={"Linear"} size={16}/> Copy Code</>
+          }
         </Button>
       </div>
       <div className="overflow-x-auto" style={{width: '100%'}}>
