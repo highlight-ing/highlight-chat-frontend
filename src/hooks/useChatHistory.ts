a71aa39bb10024e3ffdf14b93f1d899e0379f192
Galkon <Galkon@users.noreply.github.com>
2024-08-07 19:06:20 -0600

    chat history grouping + refreshing title

--- src/hooks/useChatHistory.ts
+++ src/hooks/useChatHistory.ts
@@ -7,10 +7,15 @@ interface ChatHistoryResponse {
   conversations: ChatHistoryItem[];
 }
 
+const MAX_RETRIES = 3;
+const RETRY_INTERVAL = 10000
+
 export const useChatHistory = (): {history: ChatHistoryItem[], refreshChatHistory: () => Promise<ChatHistoryItem[]>} => {
   const {get} = useApi()
   const {conversationId, history, setHistory} = useStore((state) => state);
   const initialFetchDone = useRef(false);
+  const fetchRetryRef = useRef<NodeJS.Timeout>()
+  const fetchRetryCountRef = useRef(0)
 
   const fetchResponse = async () => {
     try {
@@ -39,6 +44,32 @@ export const useChatHistory = (): {history: ChatHistoryItem[], refreshChatHistor
       // New conversation found after initial fetch
       console.log('Refreshing chat history, new conversation found');
       fetchResponse();
+    } else {
+      // If the latest conversation title is "New Conversation"
+      const chat = history.find(chat => chat.id === conversationId)
+      if (chat?.title === 'New Conversation' && fetchRetryCountRef.current < MAX_RETRIES) {
+        console.log(`Refreshing chat history until title is provided, ${MAX_RETRIES - fetchRetryCountRef.current} tries remaining`)
+
+        // Retry until title is assigned
+        fetchRetryRef.current = setTimeout(() => {
+          fetchResponse()
+            .then((conversations) => {
+              const chat = conversations.find(chat => chat.id === conversationId)
+              if (chat && chat.title !== 'New Conversation') {
+                fetchRetryCountRef.current = 0
+                console.log('Reset history retry counter')
+              }
+            })
+          fetchRetryRef.current = undefined
+          fetchRetryCountRef.current++
+        }, RETRY_INTERVAL)
+      }
+    }
+
+    return () => {
+      if (fetchRetryRef.current) {
+        clearInterval(fetchRetryRef.current)
+      }
     }
   }, [history, conversationId]);
 
