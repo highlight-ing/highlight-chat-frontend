14816f1d4fac4c09b0b6f06310879ec852e6525e
Julian <me@julianpryce.com>
2024-08-12 16:15:38 -0400

    Add prompt app selection page

---
+++ src/app/(app)/prompts/[external_id]/page.tsx
@@ -0,0 +1,26 @@
+import HighlightChat from "@/components/HighlightChat/HighlightChat";
+import PromptHelper from "@/components/HighlightChat/PromptHelper";
+import { supabaseAdmin } from "@/lib/supabase";
+
+export default async function Home({
+  params,
+}: {
+  params: { external_id: string };
+}) {
+  // fetch the prompt app from supabase
+  const { data: prompt, error } = await supabaseAdmin
+    .from("prompts")
+    .select("*")
+    .eq("external_id", params.external_id)
+    .maybeSingle();
+
+  if (error) {
+    return <div>Error fetching prompt: {error.message}</div>;
+  }
+
+  if (!prompt) {
+    return <div>Prompt not found</div>;
+  }
+
+  return <PromptHelper prompt={prompt} />;
+}
