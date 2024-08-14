6ed425b8a5bd68af89fddecc1d270c0913355ce3
Atharva Vaidya <atharva.vaidya@icloud.com>
2024-08-14 13:27:02 +0100

    feat: adding events to queue while Amplitude is not initialised

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,26 +1,44 @@
 import * as amplitude from '@amplitude/analytics-browser';
 
+// Define a type for the event properties
+interface EventProperties {
+  [key: string]: any;
+}
 
- // Define a type for the event properties
- interface EventProperties {
-   [key: string]: any;
- }
+interface QueuedEvent {
+  eventType: string;
+  eventProperties: EventProperties;
+}
 
- export const initAmplitude = (userId: string): void => {
-   if (typeof window !== 'undefined') {
-     amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY as string, {
-       defaultTracking: true,
-       userId: userId,
-     });
-   }
- };
+let isInitialized = false;
+const eventQueue: QueuedEvent[] = [];
 
- const trackEventInternal = (eventType: string, eventProperties: EventProperties): void => {
-   if (typeof window !== 'undefined') {
-     amplitude.track(eventType, eventProperties);
-   }
- };
+export const initAmplitude = (userId: string): void => {
+  if (typeof window !== 'undefined') {
+    amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY as string, {
+      defaultTracking: true,
+      userId: userId,
+    });
+    isInitialized = true;
+    
+    // Send queued events
+    eventQueue.forEach(event => {
+      amplitude.track(event.eventType, event.eventProperties);
+    });
+    eventQueue.length = 0; // Clear the queue
+  }
+};
 
- export const trackEvent = (eventType: string, eventProperties: EventProperties): void => {
-   trackEventInternal(eventType, eventProperties);
- }
+const trackEventInternal = (eventType: string, eventProperties: EventProperties): void => {
+  if (typeof window !== 'undefined') {
+    if (isInitialized) {
+      amplitude.track(eventType, eventProperties);
+    } else {
+      eventQueue.push({ eventType, eventProperties });
+    }
+  }
+};
+
+export const trackEvent = (eventType: string, eventProperties: EventProperties): void => {
+  trackEventInternal(eventType, eventProperties);
+}
