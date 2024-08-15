63ffd9c743a38c892906ef29ac6237976e3af116
Galkon <Galkon@users.noreply.github.com>
2024-08-14 21:51:05 -0600

    remove promptvariablebutton, use ghost-neutral variant button

--- src/components/prompts/PromptEditor/PromptInput.tsx
+++ src/components/prompts/PromptEditor/PromptInput.tsx
@@ -1,17 +1,7 @@
 import CodeEditor from '@uiw/react-textarea-code-editor'
 import { Gallery, Monitor, Sound, User } from 'iconsax-react'
-
-function PromptVariableButton({ name, icon, onClick }: { name: string; icon: React.ReactNode; onClick: () => void }) {
-  return (
-    <div
-      className="text-light-40 flex flex-row items-center space-x-2 rounded-md px-2 py-1 hover:cursor-pointer hover:bg-light-20"
-      onClick={onClick}
-    >
-      <div>{icon}</div>
-      <div>{name}</div>
-    </div>
-  )
-}
+import Button from "@/components/Button/Button";
+import variables from '@/variables.module.scss'
 
 /**
  * PromptInput is a component that allows the user to input a prompt with
@@ -32,27 +22,39 @@ export default function PromptInput({
 
   return (
     <>
-      <div className="flex flex-row space-x-4 py-3 pb-3">
-        <PromptVariableButton
-          name="Image"
-          icon={<Gallery variant="Bold" size={16} color="#FF2099" />}
+      <div className="flex flex-row gap-2 py-3 pb-3">
+        <Button
+          size={'medium'}
+          variant={'ghost-neutral'}
           onClick={() => onVariableClick('{{image}}')}
-        />
-        <PromptVariableButton
-          name="Screen"
-          icon={<Monitor variant="Bold" size={16} color="#FF2099" opacity="60%" />}
+        >
+          <Gallery variant="Bold" size={16} color="#FF2099" />
+          Image
+        </Button>
+        <Button
+          size={'medium'}
+          variant={'ghost-neutral'}
           onClick={() => onVariableClick('{{screen}}')}
-        />
-        <PromptVariableButton
-          name="Audio"
-          icon={<Sound variant="Bold" size={16} color="#4CEDA0" opacity="60%" />}
+        >
+          <Monitor variant="Bold" size={16} color="#FF2099" />
+          Screen
+        </Button>
+        <Button
+          size={'medium'}
+          variant={'ghost-neutral'}
           onClick={() => onVariableClick('{{audio}}')}
-        />
-        <PromptVariableButton
-          name="About Me"
-          icon={<User variant="Bold" size={16} color="#ECFF0C" opacity="60%" />}
+        >
+          <Sound variant="Bold" size={16} color={variables.green60} />
+          Audio
+        </Button>
+        <Button
+          size={'medium'}
+          variant={'ghost-neutral'}
           onClick={() => onVariableClick('{{about_me}}')}
-        />
+        >
+          <User variant="Bold" size={16} color={'#ECFF0C'} />
+          About Me
+        </Button>
       </div>
       <div>
         <CodeEditor
