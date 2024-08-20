506364f0ceb4ea80b37d42d2d96c25461ce8fb18
Atharva Vaidya <AtharvaVaidya@users.noreply.github.com>
2024-08-20 14:38:58 +0100

    Revert "Fix/first time loading indicator"

--- src/stores/messages.ts
+++ src/stores/messages.ts
@@ -4,7 +4,6 @@ import { Message } from '@/types'
 export interface MessagesState {
   messages: Message[]
   isLoadingMessages: boolean
-  isChatting: boolean
 }
 
 export type MessagesSlice = MessagesState & {
@@ -12,20 +11,17 @@ export type MessagesSlice = MessagesState & {
   updateLastMessage: (message: Message) => void
   clearMessages: () => void
   setIsLoadingMessages: (isLoading: boolean) => void
-  setIsChatting: (isChatting: boolean) => void
 }
 
 export const initialMessagesState: MessagesState = {
   messages: [],
   isLoadingMessages: false,
-  isChatting: false,
 }
 
 export const createMessagesSlice: StateCreator<MessagesSlice> = (set) => ({
   ...initialMessagesState,
   addMessage: (message: Message) => set((state) => ({ messages: [...state.messages, message] })),
   updateLastMessage: (message: Message) => set((state) => ({ messages: [...state.messages.slice(0, -1), message] })),
-  clearMessages: () => set({ messages: [], isChatting: false }),
+  clearMessages: () => set({ messages: [] }),
   setIsLoadingMessages: (isLoading: boolean) => set({ isLoadingMessages: isLoading }),
-  setIsChatting: (isChatting: boolean) => set({ isChatting }),
 })
