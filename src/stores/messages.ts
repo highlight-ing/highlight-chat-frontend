43e18bf79d62568427a81c0e61b49019d9f9b955
Atharva Vaidya <atharva.vaidya@icloud.com>
2024-08-20 12:44:32 +0100

    feat: add loading indicator when loading historical convo

--- src/stores/messages.ts
+++ src/stores/messages.ts
@@ -3,16 +3,19 @@ import { Message } from '@/types'
 
 export interface MessagesState {
   messages: Message[]
+  isLoadingMessages: boolean
 }
 
 export type MessagesSlice = MessagesState & {
   addMessage: (message: Message) => void
   updateLastMessage: (message: Message) => void
   clearMessages: () => void
+  setIsLoadingMessages: (isLoading: boolean) => void
 }
 
 export const initialMessagesState: MessagesState = {
   messages: [],
+  isLoadingMessages: false,
 }
 
 export const createMessagesSlice: StateCreator<MessagesSlice> = (set) => ({
@@ -20,4 +23,5 @@ export const createMessagesSlice: StateCreator<MessagesSlice> = (set) => ({
   addMessage: (message: Message) => set((state) => ({ messages: [...state.messages, message] })),
   updateLastMessage: (message: Message) => set((state) => ({ messages: [...state.messages.slice(0, -1), message] })),
   clearMessages: () => set({ messages: [] }),
+  setIsLoadingMessages: (isLoading: boolean) => set({ isLoadingMessages: isLoading }),
 })
