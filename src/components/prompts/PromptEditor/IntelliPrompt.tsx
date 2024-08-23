334a18edbf5c614014e4a82429d40072beef7872
Julian <me@julianpryce.com>
2024-08-22 21:31:31 -0400

    Update IntelliPrompt.tsx

--- src/components/prompts/PromptEditor/IntelliPrompt.tsx
+++ src/components/prompts/PromptEditor/IntelliPrompt.tsx
@@ -3,48 +3,21 @@ import styles from './prompteditor.module.scss'
 import Button from '@/components/Button/Button'
 import { Gallery, Monitor, Sound, User, Windows } from 'iconsax-react'
 import variables from '@/variables.module.scss'
-import { useRef } from 'react'
-import { editor } from 'monaco-editor'
-
-// function createDependencyProposals(range: any) {
-//     // returning a static list of proposals, not even looking at the prefix (filtering is done by the Monaco editor),
-//     // here you could do a server side lookup
-//     return [
-//       {
-//         label: '"lodash"',
-//         kind: monaco.languages.CompletionItemKind.Function,
-//         documentation: 'The Lodash library exported as Node.js modules.',
-//         insertText: '"lodash": "*"',
-//         range: range,
-//       },
-//       {
-//         label: '"express"',
-//         kind: monaco.languages.CompletionItemKind.Function,
-//         documentation: 'Fast, unopinionated, minimalist web framework',
-//         insertText: '"express": "*"',
-//         range: range,
-//       },
-//       {
-//         label: '"mkdirp"',
-//         kind: monaco.languages.CompletionItemKind.Function,
-//         documentation: 'Recursively mkdir, like <code>mkdir -p</code>',
-//         insertText: '"mkdirp": "*"',
-//         range: range,
-//       },
-//       {
-//         label: '"my-third-party-library"',
-//         kind: monaco.languages.CompletionItemKind.Function,
-//         documentation: 'Describe your library here',
-//         insertText: '"${1:my-third-party-library}": "${2:1.2.3}"',
-//         insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-//         range: range,
-//       },
-//     ]
-//   }
+import { useEffect, useRef, useState } from 'react'
+import { editor, IDisposable } from 'monaco-editor'
+import { buildSuggestions } from '@/lib/IntelliPrompt'
 
 export default function IntelliPrompt() {
   const monacoRef = useRef<Monaco | undefined>()
   const editorRef = useRef<editor.IStandaloneCodeEditor | undefined>()
+
+  const [completionDisposable, setCompletionDisposable] = useState<IDisposable>()
+
+  useEffect(() => {
+    return () => {
+      completionDisposable?.dispose()
+    }
+  }, [completionDisposable])
 
   function handleEditorWillMount(monaco: Monaco) {
     monacoRef.current = monaco
@@ -64,81 +37,29 @@ export default function IntelliPrompt() {
       },
     })
 
-    monaco.languages.registerCompletionItemProvider('handlebars', {
-      triggerCharacters: ['{'], // Trigger when typing `{`
+    const disposer = monaco.languages.registerCompletionItemProvider('handlebars', {
+      triggerCharacters: ['{', '.'],
+      provideCompletionItems: (model, position) => {
+        const suggestions = buildSuggestions(
+          monaco,
+          [
+            {
+              label: 'image',
+              insertText: 'image',
+              description: 'Bruh',
+            },
+          ],
+          model,
+          position,
+        )
 
-      provideCompletionItems: function (model, position) {
-        const textUntilPosition = model.getValueInRange({
-          startLineNumber: position.lineNumber,
-          startColumn: 1,
-          endLineNumber: position.lineNumber,
-          endColumn: position.column,
-        })
-
-        // Ensure the completion is triggered after {{
-        if (!textUntilPosition.endsWith('{{')) {
-          return { suggestions: [] }
+        return {
+          suggestions,
         }
-
-        // Define the suggestions for `{{text}}` and `{{audio}}`
-        const suggestions = [
-          {
-            label: '{{image}}',
-            kind: monaco.languages.CompletionItemKind.Snippet,
-            insertText: 'image}}',
-            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-            range: {
-              startLineNumber: position.lineNumber,
-              startColumn: position.column - 1,
-              endLineNumber: position.lineNumber,
-              endColumn: position.column,
-            },
-            documentation: '{{image}} will be replaced with the text pulled from the image.',
-          },
-          {
-            label: '{{audio}}',
-            kind: monaco.languages.CompletionItemKind.Snippet,
-            insertText: 'audio}}',
-            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-            range: {
-              startLineNumber: position.lineNumber,
-              startColumn: position.column - 1,
-              endLineNumber: position.lineNumber,
-              endColumn: position.column,
-            },
-            documentation: '{{audio}} will be replaced with the text pulled from the audio.',
-          },
-          {
-            label: '{{screen}}',
-            kind: monaco.languages.CompletionItemKind.Snippet,
-            insertText: 'screen}}',
-            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-            range: {
-              startLineNumber: position.lineNumber,
-              startColumn: position.column - 1,
-              endLineNumber: position.lineNumber,
-              endColumn: position.column,
-            },
-            documentation: '{{screen}} will be replaced with the text pulled from the screen.',
-          },
-          {
-            label: '{{about_me}}',
-            kind: monaco.languages.CompletionItemKind.Snippet,
-            insertText: 'about_me}}',
-            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-            range: {
-              startLineNumber: position.lineNumber,
-              startColumn: position.column - 1,
-              endLineNumber: position.lineNumber,
-              endColumn: position.column,
-            },
-            documentation: '{{about_me}} will be replaced with the text pulled from the about me.',
-          },
-        ]
-
-        return { suggestions: suggestions }
       },
     })
+
+    setCompletionDisposable(disposer)
   }
 
   function handleEditorDidMount(editor: editor.IStandaloneCodeEditor, monaco: Monaco) {
