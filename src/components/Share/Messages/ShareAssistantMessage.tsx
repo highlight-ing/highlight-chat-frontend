b14f1d76a5b3d6a448989b5980bda726c5f940ec
Maximilian Maksutovic <maksutovic@gmail.com>
2024-08-25 17:46:30 -0700

    Added vertical margin between CodeBlock and Markdown

--- src/components/Share/Messages/ShareAssistantMessage.tsx
+++ src/components/Share/Messages/ShareAssistantMessage.tsx
@@ -35,7 +35,9 @@ export const ShareAssistantMessage: React.FC<ShareAssistantMessageProps> = ({ me
               code({ node, inline, className, children, ...props }) {
                 const match = /language-(\w+)/.exec(className || '')
                 return !inline && match ? (
-                  <CodeBlock language={match[1]}>{children}</CodeBlock>
+                  <div className="my-4">
+                    <CodeBlock language={match[1]}>{children}</CodeBlock>
+                  </div>
                 ) : (
                   <code className={className} {...props}>
                     {children}
