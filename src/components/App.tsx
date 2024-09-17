084c6adfd9fbd0a75f1e98075d096fa1d792c97b
Atharva Vaidya <atharva.vaidya@icloud.com>
2024-09-17 19:15:25 -0400

    feat: add sentry user id to track

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -163,6 +163,7 @@ export default function App({ children }: { children: React.ReactNode }) {
   const router = useRouter()
   const { getAccessToken } = useAuth()
   const setUserId = useStore((state) => state.setUserId)
+  const userId = useStore((state) => state.userId)
 
   const navigateToNewChat = useCallback(() => {
     if (pathname !== '/') {
@@ -220,6 +221,8 @@ export default function App({ children }: { children: React.ReactNode }) {
         replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
         replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
       })
+
+      Sentry.setUser({ id: userId })
     }
 
     initializeAmplitude()
