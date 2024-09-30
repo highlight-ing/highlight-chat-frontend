4d4782b3bb797e8b24aaa9ce6901893e9b17d366
Galkon <Galkon@users.noreply.github.com>
2024-09-30 16:52:10 -0600

    unpin prompt DNS option

--- src/presentations/modals/UnpinPromptModal.tsx
+++ src/presentations/modals/UnpinPromptModal.tsx
@@ -15,37 +15,12 @@ export interface UnpinPromptModalContext {
 
 export default function UnpinPromptModal({ id, context }: ModalObjectProps) {
   const { prompt } = context as UnpinPromptModalContext
-  const { getAccessToken } = useAuth()
-  const { refreshPinnedPrompts } = usePromptApps()
-
-  const addToast = useStore((state) => state.addToast)
-
+  const { unpinPrompt } = useUnpinPromptAction()
   const closeModal = useStore((state) => state.closeModal)
 
   const onUpdate = async () => {
-    const authToken = await getAccessToken()
-    const res = await removePromptFromUser(prompt.external_id, authToken)
-
-    if (res && res.error) {
-      console.error('Error while calling removePromptFromUser', res.error)
-      return
-    }
-
-    Highlight.appStorage.delete(`ctas.promptAdded.${prompt.external_id}`)
-    refreshPinnedPrompts()
-
-    trackEvent('HL Prompt Unpinned', {
-      prompt_id: prompt.external_id,
-    })
-
+    unpinPrompt(prompt)
     closeModal(id)
-
-    addToast({
-      title: 'Action unpinned',
-      description: 'Your action has been unpinned.',
-      type: 'success',
-      timeout: 1500,
-    })
   }
 
   return (
@@ -61,8 +36,47 @@ export default function UnpinPromptModal({ id, context }: ModalObjectProps) {
         label: 'Cancel',
         onClick: () => closeModal(id),
       }}
+      doNotShowAgainOption={true}
     >
       <div>Unpinning this action will remove it from your pinned actions.</div>
     </ConfirmationModal>
   )
+}
+
+export const useUnpinPromptAction = () => {
+  const { getAccessToken } = useAuth()
+  const { refreshPinnedPrompts } = usePromptApps()
+
+  const addToast = useStore((state) => state.addToast)
+
+  const unpinPrompt = async (prompt: Prompt) => {
+    const authToken = await getAccessToken()
+    const res = await removePromptFromUser(prompt.external_id, authToken)
+
+    if (res && res.error) {
+      console.error('Error while calling removePromptFromUser', res.error)
+      return
+    }
+
+    Highlight.appStorage.delete(`ctas.promptAdded.${prompt.external_id}`)
+    refreshPinnedPrompts()
+
+    trackEvent('HL Prompt Unpinned', {
+      prompt_id: prompt.external_id,
+    })
+
+    addToast({
+      title: 'Action unpinned',
+      description: 'Your action has been unpinned.',
+      type: 'success',
+      timeout: 1500,
+    })
+
+    // @ts-ignore
+    globalThis.highlight.internal.reloadPrompts()
+  }
+
+  return {
+    unpinPrompt,
+  }
 }
