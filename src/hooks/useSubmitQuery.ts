a678b93fa104ae70db34ffb4de8e640da355076a
Umut Hope YILDIRIM <umut475@gmail.com>
2024-10-25 10:56:24 -0400

    chore: remove exces tools

--- src/hooks/useSubmitQuery.ts
+++ src/hooks/useSubmitQuery.ts
@@ -253,11 +253,7 @@ export const useSubmitQuery = () => {
 
     console.log('promptApp: ', promptApp)
     try {
-      // @deprecated
       const tools = {
-        get_more_context_from_window: false,
-        get_more_context_from_conversation: false,
-        add_or_update_about_me_facts: false,
         create_linear_ticket:
           (promptApp?.linear_integration_enabled ?? false) || (toolOverrides?.create_linear_ticket ?? false),
         create_notion_page:
@@ -328,7 +324,7 @@ export const useSubmitQuery = () => {
               value: conversation_data.transcript,
               duration: Math.floor(
                 (new Date(conversation_data.endedAt).getTime() - new Date(conversation_data.startedAt).getTime()) /
-                60000,
+                  60000,
               ),
             })
           } else {
