b34c82d0b0714341285abe492c9e362f9d25b914
vaunblu <vaunblu@proton.me>
2024-10-28 11:55:40 -0700

    chore: add highlight email to intercom

--- src/components/intercom-chat.tsx
+++ src/components/intercom-chat.tsx
@@ -1,24 +1,20 @@
 import Intercom from '@intercom/messenger-js-sdk'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 export default function IntercomChat() {
-  const [userEmail, setUserEmail] = useState('')
   useEffect(() => {
     if (window && window?.highlight) {
       const getUser = async () => {
         const highlightUserEmail = await window.highlight?.user?.getEmail()
-        setUserEmail(highlightUserEmail)
+
+        Intercom({
+          app_id: 'wrpa50zk',
+          name: highlightUserEmail,
+        })
       }
       getUser()
     }
-  }, [setUserEmail])
-
-  Intercom({
-    app_id: 'ibrkc80g',
-    // email: userEmail,
-    // user_id: user.id, // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
-    // created_at: user.createdAt, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date
-  })
+  }, [])
 
   return null
 }
