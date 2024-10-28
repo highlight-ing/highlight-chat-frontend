57c56f6262ed016eee58961a21f6200bff2ed5a6
vaunblu <vaunblu@proton.me>
2024-10-28 09:05:39 -0700

    feat: add user email to intercom

--- src/components/intercom-chat.tsx
+++ src/components/intercom-chat.tsx
@@ -1,8 +1,23 @@
 import Intercom from '@intercom/messenger-js-sdk'
+import { useEffect, useState } from 'react'
 
 export default function IntercomChat() {
+  const [userEmail, setUserEmail] = useState('')
+  useEffect(() => {
+    if (window && window?.highlight) {
+      const getUser = async () => {
+        const highlightUserEmail = await window.highlight?.user?.getEmail()
+        setUserEmail(highlightUserEmail)
+      }
+      getUser()
+    }
+  }, [setUserEmail])
+
   Intercom({
     app_id: 'ibrkc80g',
+    // email: userEmail,
+    // user_id: user.id, // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
+    // created_at: user.createdAt, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date
   })
 
   return null
