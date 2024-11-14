6b6ece2d6001278f8815e15df7f297a00c1d606d
Sam Pelletier <samapelletier@gmail.com>
2024-11-13 21:38:51 -0500

    dont use staging backend (#456)

--- src/utils/chatBackendUrl.ts
+++ src/utils/chatBackendUrl.ts
@@ -1,6 +1,8 @@
 const appVersion = typeof window !== 'undefined' ? window.highlight?.version : undefined
 const isAlpha = appVersion?.endsWith('alpha')
 
-export const backendUrl = isAlpha
-  ? 'https://chat-backend-staging.highlight.ing'
-  : (process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8080')
+// export const backendUrl = isAlpha
+//   ? 'https://chat-backend-staging.highlight.ing'
+//   : (process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8080')
+
+export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8080'
