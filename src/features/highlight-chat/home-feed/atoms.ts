2f560f44d6cf71fa067cbf16415f50ce0cec954e
vaunblu <vaunblu@proton.me>
2024-12-10 13:34:37 -0800

    chore: change localstorage name

--- src/features/highlight-chat/home-feed/atoms.ts
+++ src/features/highlight-chat/home-feed/atoms.ts
@@ -3,6 +3,6 @@
 import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
-export const feedHiddenAtom = atomWithStorage('homeFeedVisibility', false)
+export const feedHiddenAtom = atomWithStorage('hideHomeFeed', false)
 
 export const toggleFeedVisibilityAtom = atom(null, (_, set) => set(feedHiddenAtom, (previousValue) => !previousValue))
