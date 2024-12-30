dfbc305c2c77abf3f776976e340e5912d6d1d545
Julian <me@julianpryce.com>
2024-12-30 17:23:12 -0500

    Update routes

--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -4,7 +4,7 @@ import createClient from 'openapi-fetch'
 import type { paths } from '@/types/backend-schema'
 
 const client = createClient<paths>({
-  baseUrl: backendUrl,
+  baseUrl: `${backendUrl}/api/v4`,
 })
 
 export default client
