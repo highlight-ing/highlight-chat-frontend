d62fdd691b83e231111993f69bf018169a28d5d6
vaunblu <vaunblu@proton.me>
2025-01-07 09:20:37 -0800

    chore: remove console logs for appVersion and conversations

--- src/utils/appVersion.ts
+++ src/utils/appVersion.ts
@@ -1,4 +1,3 @@
 export const appVersion = typeof window !== 'undefined' ? window.highlight?.version : undefined
 export const isDevelopment = process.env.NODE_ENV === 'development'
 export const isAlpha = !isDevelopment && appVersion?.endsWith('alpha')
-console.log({ appVersion, isDevelopment, isAlpha })
